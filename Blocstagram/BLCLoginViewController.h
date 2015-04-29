7cce4dddac80b8a860913422c1f2564f4d93c6c2
Larry Lynn <llynn@takata.com>
2015-04-28 23:22:48 -0400

    Add Instagram Login

---
+++ Blocstagram/BLCLoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  BLCLoginViewController.h
+//  Blocstagram
+//
+//  Created by Larry Lynn on 4/24/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCLoginViewController : UIViewController
+
+extern NSString *const BLCLoginViewControllerDidGetAccessTokenNotification;
+
+@end
---
+++ Blocstagram/BLCLoginViewController.m
@@ -0,0 +1,118 @@
+//
+//  BLCLoginViewController.m
+//  Blocstagram
+//
+//  Created by Larry Lynn on 4/24/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "BLCLoginViewController.h"
+#import "BLCDataSource.h"
+
+@interface BLCLoginViewController () <UIWebViewDelegate>
+
+@property (nonatomic, weak) UIWebView *webView;
+
+@end
+
+@implementation BLCLoginViewController
+
+NSString *const BLCLoginViewControllerDidGetAccessTokenNotification = @"BLCLoginViewControllerDidGetAccessTokenNotification";
+
+- (NSString *)redirectURI {
+    return @"http://bloc.io";
+    
+}
+
+- (void)loadView {
+    UIWebView *webView = [[UIWebView alloc] init];
+    webView.delegate = self;
+    
+    self.webView = webView;
+    self.view = webView;
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSString *urlString = [NSString stringWithFormat:@"https://instagram.com/oauth/authorize/?client_id=%@&redirect_uri=%@&response_type=token", [BLCDataSource instagramClientID], [self redirectURI]];
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    if (url) {
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+        [self.webView loadRequest:request];
+        
+    }
+    
+    // sets Navigation Bar title to say "Login"
+    
+    self.title = NSLocalizedString(@"Login", @"Login");
+
+}
+
+// creates a Back button in top left of Navigation bar to return to login if user pushes the "Forgot password" 
+
+- (void) viewWillAppear:(BOOL)animated {
+    
+    [super viewWillAppear:animated];
+    
+    UIBarButtonItem *homeButton = [[UIBarButtonItem alloc] initWithTitle:@"Home" style:UIBarButtonItemStylePlain target:self action:@selector(Home)];
+    self.navigationItem.leftBarButtonItem = homeButton;
+}
+
+- (void) Home {
+    [[self webView] goBack];
+}
+
+
+
+- (void) dealloc {
+    //removing this line causes a weird flickering effect when you relaunch the app after loggin in, as the web view is briefly displayed, automatically authenticates with cookies, returns the access token, and dismisses the login view, sometimes in less than a second
+    [self clearInstagramCookies];
+    
+    //see https://developer.apple.com/library/ios/documentation/uikit/reference/UIWebViewDelegate_Protocol/Reference/Reference.html#//apple_ref/doc/uid/TP40006951-CH3-DontLinkElementID_1
+    
+    self.webView.delegate = nil;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) clearInstagramCookies {
+    for (NSHTTPCookie *cookie in [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]) {
+        NSRange domainRange = [cookie.domain rangeOfString:@"instagram.com"];
+        if (domainRange.location != NSNotFound) {
+            [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
+        }
+    }
+}
+
+- (BOOL) webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    NSString *urlString = request.URL.absoluteString;
+    if ([urlString hasPrefix:[self redirectURI]]) {
+        // This contains our Auth token
+        NSRange rangeOfAccessTokenParameter = [urlString rangeOfString:@"access_token="];
+        NSUInteger indexOfTokenStarting = rangeOfAccessTokenParameter.location + rangeOfAccessTokenParameter.length;
+        NSString *accessToken = [urlString substringFromIndex:indexOfTokenStarting];
+        [[NSNotificationCenter defaultCenter] postNotificationName:BLCLoginViewControllerDidGetAccessTokenNotification object:accessToken];
+        return NO;
+    }
+    
+    return YES;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
